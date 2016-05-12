352644b671de1a6522d0ac1e9a45fc7c8d76ddf3
ivan9105 <ivan9105@bk.ru>
2016-05-12 22:55:59 +0400

    first idea 1

--- src/main/java/com/sberbank/analizator/App.java
+++ src/main/java/com/sberbank/analizator/App.java
@@ -1,7 +1,8 @@
 package com.sberbank.analizator;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.sun.istack.internal.Nullable;
+
+import java.util.*;
 import java.util.regex.Pattern;
 
 /**
@@ -21,33 +22,61 @@ public class App {
         int openBracket = expression.indexOf("(");
         if (openBracket >= 0) {
             String arg = "A" + count;
+
+            List<Integer> openBracketsPos = new ArrayList<Integer>();
+            List<Integer> closedBracketsPos = new ArrayList<Integer>();
+            char[] chars = expression.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] == '(') {
+                    openBracketsPos.add(i);
+                } else if (chars[i] == ')') {
+                    closedBracketsPos.add(i);
+                }
+            }
             int closeBracket = expression.indexOf(")");
+            int shift = 0;
+            for (Integer bracketsPos : openBracketsPos) {
+                if (bracketsPos != openBracket && bracketsPos < closeBracket) {
+                    shift++;
+                }
+                if (bracketsPos > closeBracket) {
+                    break;
+                }
+            }
+
+            openBracket = openBracketsPos.get(shift);
+            closeBracket = closedBracketsPos.get(shift);
+
             if (closeBracket >= 0) {
-                String expression_ = expression.substring(openBracket, closeBracket + 1);
+                String expression_ = expression.substring(openBracket, shift == 0 ? closeBracket + 1 : closeBracket);
                 expression = expression.replace(expression_, arg);
                 System.out.println(expression);
                 expressionMap.put(arg, expression_);
+
+                //Todo same method with input expression and arg name A0 = A1 + A2
                 count++;
 
-                if (!isCompleteExpression()) {
+                if (!isCompleteExpression(expression)) {
                     evaluate();
                 }
             } else {
                 throw new RuntimeException("WTF");
             }
+            return;
+        }
 
-
-        } else {
+        int multiple = expression.indexOf("*");
+        if (multiple >= 0) {
 
         }
     }
 
-    private boolean isCompleteExpression() {
+    private boolean isCompleteExpression(String expression) {
         return !expression.matches(".*[+-/()*]+.*");
     }
 
     public static void main(String[] args) {
-        String expression = "(5 + 4) + (5 + 2) + 5 * 2";
+        String expression = "(5 + 4 + (5 * 2 + (4 + 1))) + (5 + 2) + 5 * 2";
         App app = new App(expression);
         app.evaluate();
     }
